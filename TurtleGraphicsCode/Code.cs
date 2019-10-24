f4edb703c4dbe9ce9c41b3b3bc8a067a23693b11
Adam Rejnart <adam.rejnart@arbesova.local>
2019-10-24 17:04:10 +0200

    babbabab

--- TurtleGraphicsCode/Code.cs
+++ TurtleGraphicsCode/Code.cs
@@ -1,51 +1,48 @@
 ﻿using System;
 
-namespace TurtleGraphicsCode {
+namespace TurtleGraphicsCode
+{
 
-	public class Code {
+    public class Code
+    {
 
-		/// <summary>
-		/// This is the place to put your turtle code
-		/// </summary>
-		public Turtle ToExecute() {
-			Turtle t = new Turtle();
-			t.Rotate(-90);
+        /// <summary>
+        /// This is the place to put your turtle code
+        /// </summary>
+        public Turtle ToExecute()
+        {
 
-			for (int i = 0; i < 20; i++) {
-				DrawSection(t, 100);
-			}
-			return t;
-		}
+            Turtle t = new Turtle();
+            t.Rotate(-90);
 
-		void DrawSection(Turtle t, int p) {
-			t.Forward(10);
-			t.StoreTurtlePosition();
-			t.Rotate(-45);
-			t.Forward(p);
-			t.RestoreTurtlePosition();
-			t.Rotate(45);
-			t.Forward(p);
-			t.RestoreTurtlePosition(true);
-		}
-	}
+            for (int i = 0; i < 6; i++)
+            {
+                t.StoreTurtlePosition();
+
+
+                for (int j = 0; i < 20; i++)
+                {
+                    DrawSection(t, 100);
+
+                }
+
+
+                t.RestoreTurtlePosition();
+                t.Rotate(-90);
+            }
+            return t;
+        }
+
+        void DrawSection(Turtle t, int p)
+        {
+            t.Forward(10);
+            t.StoreTurtlePosition();
+            t.Rotate(-45);
+            t.Forward(p);
+            t.RestoreTurtlePosition();
+            t.Rotate(45);
+            t.Forward(p);
+            t.RestoreTurtlePosition(true);
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Vytvořím si složku na disku Z: na mé projekty ve Visual Studiu
-//Otevřu cmd/powershell v složce z řádku 11
-//Zadám 'git clone https://github.com/Michal-MK/TurtleGraphics.git'
-//Ovetřu stažený solution ve visual studiu(.sln soubor)
-//Najdu projekt 'TurteGraphicsCode'
-//Najdu soubor Code.cs a otevřu ho
-//Jsem zde!
